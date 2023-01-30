4005d763db0326e59db5e8e13dc588cd17f15406
Shadowjian <karakalel.adorable@gmail.com>
2023-01-30 21:18:05 +0800

    test

--- components/HomePage/JobsofDay.js
+++ components/HomePage/JobsofDay.js
@@ -1,56 +1,61 @@
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Image from 'next/image';
-import moment from 'moment';
-import useHooks from './hooks'
-import JobCardComponent from '../Common/JobCardComponent'
-import { useTranslation } from 'react-i18next';
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
+import Image from "next/image"
+import moment from "moment"
+import useHooks from "./hooks"
+import JobCardComponent from "../Common/JobCardComponent"
+import { useTranslation } from "react-i18next"
 import {
-  JODContainer, 
-  TitleContainer, 
-  Title, 
-  Subtitle, 
+  JODContainer,
+  TitleContainer,
+  Title,
+  Subtitle,
   JOBBodyContainer,
   CustomJobCatButton,
   CustomH1
-} from './components'
+} from "./components"
 
-const JobsofDay = ({employers, cat_list}) => {
+const JobsofDay = ({ employers, cat_list }) => {
+  const { isSelected, setIsSelected } = useHooks()
 
-  const {isSelected,
-    setIsSelected} = useHooks();
-
-  const { t:translate } = useTranslation();
-  let filterEmployers = employers.filter((employer) => employer.cat_of_industry === isSelected)
-
+  const { t: translate } = useTranslation()
+  // let filterEmployers = employers.filter(
+  //   employer => employer.cat_of_industry === isSelected
+  // )
+  // console.log(filterEmployers)
 
   return (
     <JODContainer className="mt-5">
-         <TitleContainer>
-            <Title className="mb-10">{translate('jobsoftheday')}</Title>
-            {/* <Subtitle>Aliquip ullamco duis incididunt ea nulla consectetur cillum eu eiusmod dolore cupidatat nulla do in.</Subtitle> */}
-        </TitleContainer>
-        <JOBBodyContainer>
-          <Row className="d-flex justify-content-center">
-              {!cat_list ? (
-                <CustomH1>No Category List Found!</CustomH1>
-              ):
-              (cat_list.map((category, i) => {
-                return (
-                  <CustomJobCatButton key={i} isSelected={category.title === isSelected} onClick={() => setIsSelected(category.title)}>
-                    <Image
-                        // loader={myLoader}
-                        src="/images/home-page/management.svg"
-                        alt=""
-                        width={18}
-                        height={18}
-                        quality={100}
-                    />
-                    {category.title}
-                  </CustomJobCatButton>
-                )
-              }))}
-              {/* <CustomJobCatButton variant="outline-primary">
+      <TitleContainer>
+        <Title className="mb-10">{translate("jobsoftheday")}</Title>
+        {/* <Subtitle>Aliquip ullamco duis incididunt ea nulla consectetur cillum eu eiusmod dolore cupidatat nulla do in.</Subtitle> */}
+      </TitleContainer>
+      <JOBBodyContainer>
+        <Row className="d-flex justify-content-center">
+          {!cat_list ? (
+            <CustomH1>No Category List Found!</CustomH1>
+          ) : (
+            cat_list.map((category, i) => {
+              return (
+                <CustomJobCatButton
+                  key={i}
+                  isSelected={category.title === isSelected}
+                  onClick={() => setIsSelected(category.title)}
+                >
+                  <Image
+                    // loader={myLoader}
+                    src="/images/home-page/management.svg"
+                    alt=""
+                    width={18}
+                    height={18}
+                    quality={100}
+                  />
+                  {category.title}
+                </CustomJobCatButton>
+              )
+            })
+          )}
+          {/* <CustomJobCatButton variant="outline-primary">
                 <Image
                     src="/images/home-page/management.svg"
                     alt=""
@@ -60,7 +65,7 @@ const JobsofDay = ({employers, cat_list}) => {
                 />
                 Management
               </CustomJobCatButton> */}
-              {/* <CustomJobCatButton variant="outline-primary">
+          {/* <CustomJobCatButton variant="outline-primary">
                 <Image
                     src="/images/home-page/marketing.svg"
                     alt=""
@@ -110,54 +115,53 @@ const JobsofDay = ({employers, cat_list}) => {
                   />
                 Content Writer
               </CustomJobCatButton> */}
-          </Row>
-          <Row className="mt-5">
-            {!employers ? 
-              (<CustomH1>EEMMPPTTYY</CustomH1>) :
-              (isSelected === '' ? (
-                employers.reverse().map((employer, i) => {
-                  return (
-                    <Col  key={i} className="col-12 col-md-6 col-lg-4">
-                      <JobCardComponent 
-                        keyId={employer._id}
-                        imgSrc="/images/home-page/brand-1.png" 
-                        cardTitle={employer.company_name} 
-                        location={employer.location}
-                        jobTitle={employer.occupation_title}
-                        typeOfJob={employer.job_type}
-                        timePosted={moment(employer.createdAt).fromNow()}
-                        jobDesc={employer.job_desc.replace(/(<([^>]+)>)/ig, '')}
-                        jobTags={employer.job_tags}
-                        jobSalary={`${employer.salary}`}
-                        category={employer.cat_of_industry}
-                      />
-                    </Col>
-                  )
-                })
-              ) :
-              (filterEmployers.reverse().map((employer, i) => {
-                return (
-                  <Col  key={i} className="col-12 col-md-6 col-lg-4">
-                    <JobCardComponent 
-                      keyId={employer._id}
-                      imgSrc="/images/home-page/brand-1.png" 
-                      cardTitle={employer.company_name} 
-                      location={employer.location}
-                      jobTitle={employer.occupation_title}
-                      typeOfJob={employer.job_type}
-                      timePosted={moment(employer.createdAt).fromNow()}
-                      jobDesc={employer.job_desc.replace(/(<([^>]+)>)/ig, '')}
-                      jobTags={employer.job_tags}
-                      jobSalary={`${employer.salary}`}
-                      category={employer.cat_of_industry}
-                    />
-                  </Col>
-                )
-              }))
+        </Row>
+        <Row className="mt-5">
+          {!employers ? (
+            <CustomH1>EEMMPPTTYY</CustomH1>
+          ) : isSelected === "" ? (
+            employers.reverse().map((employer, i) => {
+              return (
+                <Col key={i} className="col-12 col-md-6 col-lg-4">
+                  <JobCardComponent
+                    keyId={employer._id}
+                    imgSrc="/images/home-page/brand-1.png"
+                    cardTitle={employer.company_name}
+                    location={employer.location}
+                    jobTitle={employer.occupation_title}
+                    typeOfJob={employer.job_type}
+                    timePosted={moment(employer.createdAt).fromNow()}
+                    jobDesc={employer.job_desc.replace(/(<([^>]+)>)/gi, "")}
+                    jobTags={employer.job_tags}
+                    jobSalary={`${employer.salary}`}
+                    category={employer.cat_of_industry}
+                  />
+                </Col>
               )
-          }
-          </Row>
-        </JOBBodyContainer>
+            })
+          ) : (
+            filterEmployers.reverse().map((employer, i) => {
+              return (
+                <Col key={i} className="col-12 col-md-6 col-lg-4">
+                  <JobCardComponent
+                    keyId={employer._id}
+                    imgSrc="/images/home-page/brand-1.png"
+                    cardTitle={employer.company_name}
+                    location={employer.location}
+                    jobTitle={employer.occupation_title}
+                    typeOfJob={employer.job_type}
+                    timePosted={moment(employer.createdAt).fromNow()}
+                    jobDesc={employer.job_desc.replace(/(<([^>]+)>)/gi, "")}
+                    jobTags={employer.job_tags}
+                    jobSalary={`${employer.salary}`}
+                    category={employer.cat_of_industry}
+                  />
+                </Col>
+              )
+            })
+          )}
+        </Row>
+      </JOBBodyContainer>
     </JODContainer>
   )
 }
